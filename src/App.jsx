bba2f0a7fb7448815c131e1d64b19707aafd9c10
Ronit Suman <ronitsuman59@gmail.com>
2025-01-08 17:39:28 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import Postselection from './Component/Topics/Postselection'
 const App = () => {
   return (
     <div className=' bg-cyan-700 w-auto h-auto p-4 flex flex-col item-center align-cente gap-4 '>
-      <Header/> 
+      <Header /> 
       <Postselection/> 
 
       <Footer/>
